b808be67543a3cc132314044bfac8e8c04c1b972
TIENIT-PC\tien.nq <bktien@gmail.com>
2017-06-29 10:26:13 +0700

    Importing data for Database

--- Projects/SWM2/SWM2/Program.cs
+++ Projects/SWM2/SWM2/Program.cs
@@ -15,7 +15,7 @@ namespace SWM2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new MasterForm());
         }
     }
 }
