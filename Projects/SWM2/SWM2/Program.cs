20770951a558e63ea32db3dbff618e1386c0823c
mercury86 <caoboitap1@yahoo.com>
2017-10-26 21:13:11 +0700

    up

--- Projects/SWM2/SWM2/Program.cs
+++ Projects/SWM2/SWM2/Program.cs
@@ -18,7 +18,7 @@ namespace VKTIM
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frmGridControlCopyPaste());
             //Application.Run(new frmDmInputBill());
-            Application.Run(new frmRPInputDetail());
+            Application.Run(new frmMain());
         }
     }
 }
